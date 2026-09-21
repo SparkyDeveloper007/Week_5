232d6091b04b0a1c65a7f089696216f89cd498cc
NewGenDev11 <fisher.curtis85@gmail.com>
2026-09-20 20:59:11 -0400

    2nd project added

--- Ch10Ex1ToDoList/ToDoList/obj/Debug/net9.0/ToDoList.AssemblyInfo.cs
+++ Ch10Ex1ToDoList/ToDoList/obj/Debug/net9.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c88a699cea3f4ac23a16a6455129d93173d7a65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44ee1edb4da6d832cb293753a4e56ec99690c05f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
